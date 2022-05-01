8d2cebc4db063ec725e5961dd662cf261258e5c1
hs11346 <104689399+hs11346@users.noreply.github.com>
2022-05-02 04:39:41 +0800

    Update rpt.py

--- rpt.py
+++ rpt.py
@@ -163,7 +163,7 @@ with description:
     st.subheader("Company Details")
     comp = st.selectbox("Select Company", options=list(supplier.drop(columns = "id")[((supplier["Score Ranked by the IHG"]>=ihg) | (supplier["Score Ranked by the IHG"]!=supplier["Score Ranked by the IHG"])) & (supplier["Country"]==country)].index.values))
     st.markdown(supplier["Company description"].loc[comp])
-    st.markdown(supplier["Description of products/services"].loc[comp])
+    st.markdown("**Description of Product:** :\n"+"* "+supplier["Description of products/services"].loc[comp])
     st.markdown("\nProduct Categories include:")
     st.markdown(" ".join(supplier["Type of Products"].loc[comp]))
 
