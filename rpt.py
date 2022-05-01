50affe71aa56bbe5dd4d7f541796bdc38924ce18
hs11346 <104689399+hs11346@users.noreply.github.com>
2022-05-02 04:11:31 +0800

    Update rpt.py

--- rpt.py
+++ rpt.py
@@ -177,7 +177,7 @@ with st.sidebar:
         if re.fullmatch(regex, email):
             user = yg.SMTP(user='hbgwjti@gmail.com',password = st.secrets['pw'])
             user.send(to=email,subject = 'Webapp trial email from '+org, contents = message)
-            st.markdown("Error: Invalid Email")
+            st.markdown("Received, thanks!")
         else:
             st.markdown("Error: Invalid Email")
         
