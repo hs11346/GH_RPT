44042831b256a9cce58f943dbcb4dec5033fc71c
hs11346 <104689399+hs11346@users.noreply.github.com>
2022-05-04 01:48:46 +0800

    Update rpt.py

--- rpt.py
+++ rpt.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(
      page_title="GH_RPT App",
      layout="wide",
-     initial_sidebar_state="expanded",
+     initial_sidebar_state="centered",
      menu_items={
          'Get Help': 'https://www.greenhospitality.io',
          'Report a bug': "https://www.greenhospitality.io",
